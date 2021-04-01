b9d4c70c66ff3422b1b40ade146718b7a9e1d77a
Tino Muzambi <tinomuzambi@gmail.com>
2021-04-01 15:56:35 +0200

    Adds logic.

--- partB-question3.hs
+++ partB-question3.hs
@@ -1,3 +1,14 @@
+delete :: Int -> [Int] -> [Int]
+
+delete n [] = []
+delete n (x:xs)
+    | (n == x) = xs
+    | otherwise = x : delete n xs
+
 perms :: [Int] -> [[Int]]
 
-perms [] = []
+perms [] = [[]]
+perms xs = do { x <- xs
+            ; let l = delete x xs
+            ; ls <- perms l
+            ; return $ x : ls }
