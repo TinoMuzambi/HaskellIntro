bad31eb135ae7a7be10c2ed50b9380f4cde41c2d
Tino Muzambi <tinomuzambi@gmail.com>
2021-04-01 17:28:14 +0200

    Trying bool helper.

--- partB-question4.hs
+++ partB-question4.hs
@@ -1,8 +1,8 @@
-mySplit :: [Int] -> Int -> [([Int], [Int])]
+mySplit :: [Int] -> Int -> Bool -> [([Int], [Int])]
 
-mySplit xs 3 = [splitAt 3 xs]
-mySplit xs n  = [splitAt n xs] ++ mySplit xs (n + 1)
+mySplit xs n False = [splitAt 3 xs]
+mySplit xs n True  = [splitAt n xs] ++ mySplit xs (n + 1) True
 
 split :: [Int] -> [([Int], [Int])]
 
-split xs = mySplit xs 1 
+split xs = mySplit xs 1 True
