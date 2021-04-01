52710b45281498ddff3af9b130dbc46c1c623b61
Tino Muzambi <tinomuzambi@gmail.com>
2021-04-01 15:22:43 +0200

    Adds logic for recursive cases.

--- partB-question1.hs
+++ partB-question1.hs
@@ -8,4 +8,6 @@ eval (Add x y) = eval x + eval y
 eval (Mul x y) = eval x * eval y
 
 values :: Expr -> [Int]
-values (Val n) = n
+values (Val n) = [n]
+values (Add x y) = values x ++ values y 
+values (Mul x y) = values x ++ values y 
