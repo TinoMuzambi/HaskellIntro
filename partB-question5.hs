35da40dc755a6f6d853a9539da1118dc4bae0cd4
Tino Muzambi <tinomuzambi@gmail.com>
2021-04-06 13:44:50 +0200

    Implements exprs with helper function.

--- partB-question5.hs
+++ partB-question5.hs
@@ -1,7 +1,26 @@
-data Expr = Val Int 
-        | Add Expr Expr 
-        | Mul Expr Expr
+mySplit :: [Int] -> Int -> Bool -> [([Int], [Int])]
+
+mySplit xs n False = [splitAt n xs]
+mySplit xs n True  = if n == (length xs - 2)
+                        then [splitAt n xs] ++ mySplit xs (n + 1) False
+                    else [splitAt n xs] ++ mySplit xs (n + 1) True
+
+split :: [Int] -> [([Int], [Int])]
+
+split xs = if 2 == (length xs)
+            then mySplit xs 1 False
+        else mySplit xs 1 True
+
+data Expr = Val Int | App Op Expr Expr
+data Op = Add | Mul
 
 exprs :: [Int] -> [Expr]
-
 exprs [] = []
+exprs [n] = [Val n]
+exprs ns = [e | (ls,rs) <- split ns
+                , l <- exprs ls
+                , r <- exprs rs
+                , e <- combine l r]
+
+combine :: Expr -> Expr -> [Expr]
+combine l r = [App o l r | o <- [Add,Mul]]
