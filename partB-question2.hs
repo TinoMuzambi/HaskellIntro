cde3a924474cf4bc45a36135c90cc6df49dba01a
Tino Muzambi <tinomuzambi@gmail.com>
2021-04-08 01:27:04 +0200

    Removes old files.

--- partB-question2.hs
+++
@@ -1,6 +0,0 @@
-delete :: Int -> [Int] -> [Int]
-
-delete n [] = []
-delete n (x:xs)
-    | (n == x) = xs
-    | otherwise = x : delete n xs
--- partB-question6.hs
+++
@@ -1,8 +0,0 @@
-data Expr = Val Int 
-        | Add Expr Expr 
-        | Mul Expr Expr
-
-solve :: [Int] -> Int -> [Expr]
-solve xs x = [e | xs' <- choices xs
-                , e <- exprs xs'
-                , eval e == [x]]
