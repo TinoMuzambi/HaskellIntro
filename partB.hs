27698ddaca89acb0b0f965a6e3e557df172e37dc
Tino Muzambi <tinomuzambi@gmail.com>
2021-04-08 22:11:03 +0200

    Cleaning up.

--- partB.hs
+++ partB.hs
@@ -3,14 +3,17 @@ data Expr = Val Int | App Op Expr Expr
 data Op = Add | Mul
 
 apply :: Op -> Int -> Int -> Int
+
 apply Add x y = x + y
 apply Mul x y = x * y
 
 eval :: Expr -> Int
+
 eval (Val n) = n
 eval (App o x y) = apply o (eval x) (eval y)
 
 values :: Expr -> [Int]
+
 values (Val n) = [n]
 values (App _ x y) = values x ++ values y
 
@@ -47,9 +50,11 @@ split xs = if (length xs) == 2
 
 -- Question 5
 combine :: Expr -> Expr -> [Expr]
+
 combine x y = [App o x y | o <- [Add,Mul]]
 
 exprs :: [Int] -> [Expr]
+
 exprs [] = []
 exprs [n] = [Val n]
 exprs xs = [l | (ls,rs) <- split xs
